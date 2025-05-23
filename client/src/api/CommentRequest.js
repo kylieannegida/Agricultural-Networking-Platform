9d2afe5f1153fd69bb7788f9a853c586491a58fb
Kylie Anne Gida <kylieannegida@gmail.com>
2025-05-23 20:13:15 +0800

    Delete client directory

--- client/src/api/CommentRequest.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-
-const API = axios.create({ baseURL: 'http://localhost:4000' });
-
-// Get all comments for a specific post
-export const getComments = (postId) => API.get(`/api/posts/${postId}/comments`);
-export const addComment = (postId, commentData) => API.post(`/api/posts/${postId}/comments`, commentData);
-
-
